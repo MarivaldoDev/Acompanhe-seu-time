c2679e56a8f62c56424b40a5a743bf5b31717e3b
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-06-02 21:08:51 -0300

    new-function

--- fut.py
+++ fut.py
@@ -1,12 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-from colorama import init, Fore, Style
+from twilio.rest import Client
+
+
+def sms():
+    sid = 'AC7b4c536de947b0d4bb01a93b3dee52a1'
+    token = '92aab5db4522def3a91788dc4aca1245'
+    twilio_number = '+15513776919'
+
+    client = Client(sid, token)
+
+    with open('noticias.txt', 'r', encoding='utf-8') as arquivo:
+         for linha in arquivo:
+            client.messages.create(
+                body=f'''Notícias do {time}\n\n
+                    {linha.strip()}''',
+               from_=twilio_number,
+               to='+5581999756049' 
+            )
 
 
 time = input('Qual teu time: ')
 print('\n')
 
-init()
 
 url = f'https://ge.globo.com/futebol/times/{time}/'
 site = requests.get(url)
@@ -15,15 +31,15 @@ soup = BeautifulSoup(site.content, 'html.parser')
 
 noticias = soup.find_all('div', attrs={'class': 'feed-post bstn-item-shape type-materia'})
 
-for noticia in noticias:
-    titulo = noticia.find('a', attrs={'class': 'feed-post-link gui-color-primary gui-color-hover'})
-    resumo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
-   
-    if titulo and resumo:
-        print(Fore.LIGHTGREEN_EX + "Título: " + Style.RESET_ALL, f"{titulo.text}")
-        print(Fore.LIGHTGREEN_EX + "Resumo: " + Style.RESET_ALL, f"{resumo.text}")
-        print(Fore.LIGHTGREEN_EX + "Link da notícia: ", Fore.LIGHTBLUE_EX + f"{titulo['href']}\n" + Style.RESET_ALL)
-        print('-'*80)
-    else:
-        print(Fore.LIGHTGREEN_EX + "Título: " + Style.RESET_ALL, f"{titulo.text}")
-        print(f"Link da notícia: ", Fore.LIGHTBLUE_EX + f"{titulo['href']}\n" + Style.RESET_ALL)
+with open('noticias.txt', 'w', encoding='utf-8') as arquivo:
+    for noticia in noticias:
+        titulo = noticia.find('a', attrs={'class': 'feed-post-link gui-color-primary gui-color-hover'})
+        resumo = noticia.find('div', attrs={'class': 'feed-post-body-resumo'})
+    
+        if titulo and resumo:
+                arquivo.write(f'Título: {titulo.text}\nResumo: {resumo.text}\nLink da notícia: {titulo['href']}\n\n')
+        else:
+            arquivo.write(f'Titulo: {titulo.text}\nLink da notícia: {titulo['href']}\n\n')
+
+
+sms()
